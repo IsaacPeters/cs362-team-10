b2a78adf789dc8bacdb93fe86ce04b27e659bfbd
Evan <amayae@oregonstate.edu>
2018-11-30 10:49:08 -0800

    moveShip function/Helper

--- src/main/java/cs361/battleships/models/Ship_CaptainsQuarters.java
+++ src/main/java/cs361/battleships/models/Ship_CaptainsQuarters.java
@@ -68,6 +68,25 @@ public class Ship_CaptainsQuarters extends Ship{
         return result;
     }
 
+    public boolean moveShip(int Direction, boolean isVerticalMove){
+        List<Square> temp = getOccupiedSquares();
+
+        for (int i = 0; i < temp.size(); i++) {
+            Square tempSquare = this.getOccupiedSquares().get(i);
+            if (isVerticalMove) {
+                temp.get(i).setRow(tempSquare.getRow() + Direction);
+            } else {
+                temp.get(i).setColumn((char) (tempSquare.getColumn() + Direction));
+            }
+        }
+    }
+    //Helper to call the move ship and process the request
+    public boolean setMove()
+    {
+        moveShip();
+
+    }
+
     @JsonIgnore
     public boolean isSunk() {
         int hit_check = 0;
